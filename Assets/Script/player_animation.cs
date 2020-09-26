532f670b5608bcd22a5740c26ccfb28e46b01c23
AndyZheng430 <zhenga6@bxscience.edu>
2020-09-26 14:33:14 -0400

    enivornment added

---
+++ Assets/Script/player_animation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_animation : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Move(horizontal, vertical);
+    }
+    private void Move(float x, float y) {
+        anim.SetFloat("VelX", x);
+        anim.SetFloat("VelY", y);
+    }
+}
